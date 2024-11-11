c4565952f81bc7ec171b145a30a3bbeba558f220
Vicente Duch <vhduch@gmail.com>
2024-11-11 17:26:12 +0100

    day one commit

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors")
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const cohortsArr = require("./cohorts.json");
+const stundetsArr = require("./students.json")
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -15,6 +17,7 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(cors({origin: ['http://localhost:5173/','http://localhost:5005/']}))
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -29,6 +32,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohortsArr)
+})
+
+app.get("/api/students", (req, res) => {
+  res.json(stundetsArr)
+})
 
 // START SERVER
 app.listen(PORT, () => {
