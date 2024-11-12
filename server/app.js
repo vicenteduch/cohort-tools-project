366e400f9a6336878d56da355bd9d39dd1d68fa0
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-11-12 17:55:44 +0100

    add Student and Cohort Schemas and Models

--- server/app.js
+++ server/app.js
@@ -2,28 +2,34 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-const cors = require("cors")
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohorts-api-tools")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 const cohortsArr = require("./cohorts.json");
-const stundetsArr = require("./students.json")
+const stundetsArr = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
-
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
-app.use(cors({origin: ['http://localhost:5173/','http://localhost:5005/']}))
+app.use(cors({ origin: ["http://localhost:5173/", "http://localhost:5005/"] }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
@@ -33,12 +39,12 @@ app.get("/docs", (req, res) => {
 });
 
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohortsArr)
-})
+  res.json(cohortsArr);
+});
 
 app.get("/api/students", (req, res) => {
-  res.json(stundetsArr)
-})
+  res.json(stundetsArr);
+});
 
 // START SERVER
 app.listen(PORT, () => {
